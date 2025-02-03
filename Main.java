c7c2231b2f7d4c1ebd765d6ec3c90ce6279f0105
Mayko Ronaldo Torres Calva <1717304@alu.murciaeduca.es>
2025-02-03 10:15:22 +0100

    Introducido clase usuario y clase main(metodo mostrar usuario y crear usuario)

--- Main.java
+++ Main.java
@@ -1,13 +1,45 @@
-public class  Main{
-	public class void main(String[] args){
-		System.out.println("Hola mundo");
-		mostrarFechaHora();
-	}
+import java.util.Scanner;
 
-	public static void mostrarFechaHora(){
-		LocalDateTime  ahora = LocalDateTime.now();
-		System.out.println("Fecha y hora actual: " + ahora);
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime cantidad de usuarios:");
+        int cantidad = teclado.nextInt();
+        Usuario[] usuarios = crearUsuarios(cantidad); // Puedes cambiar el número a cualquiera
+        mostrarUsuarios(usuarios);
+    }
+
+    public static Usuario[] crearUsuarios(int cantidad) {
+        Scanner teclado = new Scanner(System.in);
+        Usuario[] usuarios = new Usuario[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+            usuarios[i] = new Usuario();
+            System.out.println("Introduce nombre del usuario " + (i + 1) + ": ");
+            usuarios[i].nombre = teclado.nextLine();
+            System.out.println("Introduce apellidos del usuario " + (i + 1) + ": ");
+            usuarios[i].apellidos = teclado.nextLine();
+            System.out.println("Introduce email del usuario " + (i + 1) + ": ");
+            usuarios[i].email = teclado.nextLine();
+        }
+        return usuarios;
+    }
+
+    
+
+    public static void mostrarUsuarios(Usuario[] usuarios) {
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println("Nombre: " + usuarios[i].nombre);
+            System.out.println("Apellidos: " + usuarios[i].apellidos);
+            System.out.println("Email: " + usuarios[i].email);
+        }
+    }
+
+
+
+
+
+
+
 
 }
 
