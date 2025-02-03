d38d3b704d88c6442a2907cbad3286f7f080da52
Mayko Ronaldo Torres Calva <1717304@alu.murciaeduca.es>
2025-02-03 09:34:32 +0100

    Metodo mostrar fecha y hora actual

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class  Main{
 	public class void main(String[] args){
 		System.out.println("Hola mundo");
+		mostrarFechaHora();
+	}
+
+	public static void mostrarFechaHora(){
+		LocalDateTime  ahora = LocalDateTime.now();
+		System.out.println("Fecha y hora actual: " + ahora);
 	}
 
 }
+
